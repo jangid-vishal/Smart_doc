2861a835bad242160771b4c4286d7aafd2f17d83
jangid-vishal <vishjangid5738@gmail.com>
2026-06-16 21:42:07 +0530

    one more

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -12,7 +12,10 @@ import { protect } from '../middleware/authMiddleware.js';
 const router = express.Router();
 
 router.post('/register', registerUser);
-router.post('/login', loginUser);
+router.post('/login', (req, res, next) => {
+  console.log('LOGIN ROUTE HIT');
+  next();
+}, loginUser);
 router.post('/logout', logoutUser);
 router.route('/profile')
   .get(protect, getUserProfile)
